2ad6babac57683b1f618536668306f28b85117ba
Andre Christoga <andrechristoga@gmail.com>
2016-09-15 09:17:40 +0700

    PHP form using get method (on progress)

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body align="center">
-	<label for="username">Username:</label>
-	<input type="text" id="username"><br>
-	<label for="password">Password:</label>
-	<input type="password" id="password"><br>
-	<button class="button button-outline">Login</button>
+	<form action="features.php" method="get">
+		<label for="username">Username:</label>
+		<input type="text" name="username"><br>
+		<label for="password">Password:</label>
+		<input type="password" name="password"><br>
+		<input class="button button-outline" type="submit" value="Login">
+	</form>
 </body>
 </html>
